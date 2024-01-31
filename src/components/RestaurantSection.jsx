d7b679e4a9e338953786612745894ce2782a4701
Poornima-Joshi <poornima.joshi776@gmail.com>
2024-01-31 12:46:22 +0530

    filter data in normal way

--- src/components/RestaurantSection.jsx
+++ src/components/RestaurantSection.jsx
@@ -3,8 +3,8 @@ import RestaurantCard from "./RestaurantCard";
 
 
 const RestaurantSection = () => {
-   const TopRestaurant = [{
-    info: {
+   let TopRestaurant = [
+    {info: {
       id: "344287",
       name: "Varalakshmi Tiffins",
       cloudinaryImageId: "8523266de1213e8ea57e5df4eedf747e",
@@ -20,15 +20,37 @@ const RestaurantSection = () => {
       sla: {
         slaString: "25-30 mins",
       },
-    }
-  }]
+    }},
+    {info: {
+      id: "344288",
+      name: "Varalakshmi Tiffins",
+      cloudinaryImageId: "8523266de1213e8ea57e5df4eedf747e",
+      locality: "Banjara Hills",
+      areaName: "Banjara Hills",
+      costForTwo: "₹150 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4.5,
+      veg: true,
+      parentId: "6482",
+      avgRatingString: "3",
+      totalRatingsString: "10K+",
+      sla: {
+        slaString: "25-30 mins",
+      },
+    }},
+    
+    
+  ]
    return (
     <>
       <h4 className="tag-name res-name">Top restaurant chains in Hyderabad</h4>
       <div className="filter mb-3">
         <button className="filter-btn"
           onClick={()=>{
-            console.log("Button Clicked");
+            TopRestaurant = TopRestaurant.filter(
+                (res) => res.info.avgRatingString > 4
+            );
+            console.log(TopRestaurant);
           }}
         >Top Rated Restaurants</button>
       </div>
